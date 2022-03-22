ca82246c944ad630e9cf5dcfc6786cae42b5cf44
LiljaK <lilja.maria.kiiski@gmail.com>
2022-03-22 12:39:00 -0700

    Working on parsing measurement.in file

--- 5_usaco_problem/2017-dec-3.cpp
+++ 5_usaco_problem/2017-dec-3.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include <string>
 #include <map>
 #include <queue>
+#include <fstream>
 
 struct Change {
 	int day;
@@ -14,55 +14,51 @@ bool operator<(const Change& a, const Change& b){
 	return !(a.day < b.day);
 }
 
+std::priority_queue<Change> parse_file(){
+	std::ifstream fint("measurement.in"); //read file
+	std::priority_queue<Change> entries;
+
+        int num_entries;
+        fint >> num_entries;
+
+	std::cout << std::to_string(num_entries) << std::endl;
+
+	return entries;
+}
+
+void write_file(int num_changes){
+	std::ofstream fout("measurement.out");
+	fout << num_changes;
+}
+
 int get_solution(std::priority_queue<Change> changes){
 	std::string current_cow = "blank"; //Current cow in display
-	int num_changes; //Number of changes in display
+	int num_changes = 0; //Number of changes in display
 
-	std::cout << std::to_string(num_changes) << std::endl;
-
-	//Map of cows to gallons
-	std::map<std::string, int> cows;
+	std::map<std::string, int> cows; //Map of cows to gallons
 	cows["bessie"] = 7;
 	cows["else"] = 7;
 	cows["mildred"] = 7;
 
-	//Loop through queue in order of days
-	while(!changes.empty()){
+	while(!changes.empty()){ //Loop through queue in order of days
 		Change cow = changes.top(); //Newest cow
 
-		//Add gallons to corresponding cow
-		cows[cow.cow_name]+=cow.change_in_gallons;
+		cows[cow.cow_name]+=cow.change_in_gallons; //Add gallons to corresponding cow
 
 		//If cow is NOT current cow and cow's gallons are greater than current cow
 		if (cow.cow_name != current_cow && cows[cow.cow_name] >= cows[current_cow]){
 			current_cow = cows[cow.cow_name];
 			num_changes++;
 		}
+
+		//One other possibility is that if cow's gallons go down, now a different cow is in the lad. Check for this!
+
 		changes.pop();
 	}
 	return num_changes;
 }
 
-int main(){
-	Change c1;
-	c1.day = 7; c1.cow_name = "mildred"; c1.change_in_gallons = +3;
-
-	Change c2;
-        c2.day = 4; c2.cow_name = "elsie"; c2.change_in_gallons = -1;
-
-	Change c3;
-        c3.day = 9; c3.cow_name = "mildred"; c3.change_in_gallons = -1;
-
-	Change c4;
-        c4.day = 1; c4.cow_name = "bessie"; c4.change_in_gallons = +2;
-
-	std::priority_queue<Change> changes;
-	changes.push(c1);
-	changes.push(c2);
-	changes.push(c3);
-	changes.push(c4);
-
-	std::cout << std::to_string(get_solution(changes)) << std::endl;
-
+int main() {
+	parse_file();
 	return 0;
 }
