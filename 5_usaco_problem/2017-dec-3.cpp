3ad4cebe9dada38280bdd7e08915da96bebcc8bd
LiljaK <lilja.maria.kiiski@gmail.com>
2022-03-21 22:05:09 -0700

    Cow class created

--- 5_usaco_problem/2017-dec-3.cpp
+++ 5_usaco_problem/2017-dec-3.cpp
@@ -1,15 +1,23 @@
 #include <string>
 
-struct Cow {
-	string name;
-	int change;
-}
+class Cow {
 
-int get_solution(){
-	
+	public:
+		std::string name;
+		int gallons;
 
+		Cow(std::string _name){
+			name = _name;
+			gallons = 7;
+		}
+};
 
+int get_solution(Cow cows[]){
+	Cow bessie("bessie");
+	Cow elsie("elsie");
+	Cow mildred("mildred");
 
+	return 0;
 }
 
 
