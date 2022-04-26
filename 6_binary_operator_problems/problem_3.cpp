b205171107a882d2ee4c318ba45ce81ad1270880
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2022-04-26 12:37:23 -0700

    Figured out problem 3

--- 6_binary_operator_problems/problem_3.cpp
+++ 6_binary_operator_problems/problem_3.cpp
@@ -1,9 +1,15 @@
-int main(){
-	
+#include <iostream>
 
-	return 0;
-}
+using namespace std;
 
 bool number_is_odd(int num){
-	//...	
+        if (num&1){ //Last digit is 1, so odd
+                return true;
+        }
+
+        return false;
+}
+
+int main(){
+	return 0;
 }
