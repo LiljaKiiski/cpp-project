31a5382b570d4fb25eccec1574db327a25ff0857
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2022-04-26 17:47:50 -0700

    Getting started on problem 2, added examples to problem 3

--- 6_binary_operator_problems/problem_2.cpp
+++ 6_binary_operator_problems/problem_2.cpp
@@ -11,10 +11,5 @@ bool number_is_odd(int num){
 }
 
 int main(){
-	for (int x = 0; x < 10; x++){
-		string is_odd = number_is_odd(x) ? "true" : "false";
-		cout << x << " is odd: " << is_odd << endl;
-	}
-
 	return 0;
 }
