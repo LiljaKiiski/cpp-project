617b541bb67d3025f5eec016d52bf2e6729f6568
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2022-05-01 22:45:23 -0700

    Still stuck on how to do this

--- 6_binary_operator_problems/problem_1.cpp
+++ 6_binary_operator_problems/problem_1.cpp
@@ -2,17 +2,16 @@
 
 using namespace std;
 
-//01
-//1010
-
-//Switch 1 and 10...
-
 void swap (int* num1, int* num2){
+	
 	
 }
 
 int main(){
+	int num1 = 6;
+	int num2 = 3;
 	
+	swap(&num1, &num2);
 
 	return 0;
 }
