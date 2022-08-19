942f235c24f018729386cbc11d658e6a5b9ecb41
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2022-08-19 12:16:09 -0700

    -nan for -5, -3, and -1

--- quadratic-solver/main.cpp
+++ quadratic-solver/main.cpp
@@ -4,16 +4,26 @@
 double getX1();
 double getX2();
 
-double a = 6;
-double b = 4;
-double c = -2;
+double a;
+double b;
+double c;
 
 using namespace std;
 
 int main(){
+	cout << "a: ";
+	cin >> a;
+
+	cout << "b: ";
+	cin >> b;
+
+	cout << "c: ";
+	cin >> c;
 
 	cout << "The equation is: ";
 	cout << a << "x² + " << b << "x + " << c << endl; 
+
+	cout << a + b + c << endl;
 
 	cout << "X is either " << getX1() << " or "<< getX2() << endl;
 
