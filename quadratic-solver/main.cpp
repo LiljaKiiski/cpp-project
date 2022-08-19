f761eab95e8ede2d341ef758bb8b7dfeb3817c8d
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2022-08-19 12:09:25 -0700

    Works with 6x**2 + 4x -2

--- quadratic-solver/main.cpp
+++ quadratic-solver/main.cpp
@@ -1,5 +1,37 @@
+#include <iostream>
+#include <cmath>
+
+double getX1();
+double getX2();
+
+double a = 6;
+double b = 4;
+double c = -2;
+
+using namespace std;
 
 int main(){
 
+	cout << "The equation is: ";
+	cout << a << "x² + " << b << "x + " << c << endl; 
+
+	cout << "X is either " << getX1() << " or "<< getX2() << endl;
+
 	return 0;
+}
+
+double getX1(){
+	//Step one - simplify root
+	double root = sqrt( pow(b, 2) - 4 * a * c );
+
+	//Step 2 a) - Solve with + 
+	return (-b + root) / (2 * a);
+}
+
+double getX2(){
+        //Step one - simplify root
+        double root = sqrt( pow(b, 2) - 4 * a * c );
+
+        //Step 2 a) - Solve with -
+        return (-b - root) / (2 * a);
 }
